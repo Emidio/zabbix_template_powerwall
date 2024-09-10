c01b01f0bb2414cea702a39fa8bafc6a8f393b23
Emidio <emidio.reggiani@gmail.com>
2024-09-10 11:26:00 +0200

    Update powerwall.sh
    Use venv in python.

--- externalscripts/powerwall.sh
+++ externalscripts/powerwall.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-python /usr/lib/zabbix/externalscripts/powerwall.py $1 $2 $3 $4|sed "s/'/\"/g"|sed 's/True/true/g'|sed 's/False/false/g'|sed ':a;$!{N;s/\n/ /;ba;}'
+# use your venv path for python (changes in latest updates)
+cd /usr/lib/zabbix/externalscripts
+/var/www/scripts/venv/bin/python /usr/lib/zabbix/externalscripts/powerwall.py $1 $2 $3 $4|sed "s/'/\"/g"|sed 's/True/true/g'|sed 's/False/false/g'|sed ':a;$!{N;s/\n/ /;ba;}'
